b4289d90a465ae0634b54643d89a8eb8341a17dc
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-05-30 13:46:49 +0200

    Add project files.

---
+++ Particles/Particles.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "SDL.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL init failed." << endl;
+		return 1;
+	}
+
+	cout << "SDL Init succeeded." << endl;
+	SDL_Quit();
+	return 0;
+}
