6c7023aee883e359dd22e9ad316f2cc8273f6287
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-05-31 17:34:53 +0200

    Refactored screen into an object.

--- Particles/Particles.cpp
+++ Particles/Particles.cpp
@@ -1,87 +1,27 @@
 #include <iostream>
 #include "SDL.h"
+#include "Screen.h"
 
 using namespace std;
-
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+using namespace particle;
 
 int main(int argc, char* argv[])
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	Screen screen;
+	if (screen.init() == false)
 	{
-		cout << "SDL init failed." << endl;
-		return 1;
+		cout << "Error initializing screen." << endl;
 	}
-
-	SDL_Window* window = SDL_CreateWindow(
-		"Particle Fire Explosion",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH,
-		SCREEN_HEIGHT,
-		SDL_WINDOW_SHOWN
-	);
-
-	if (window == NULL)
+	
+	while (true)
 	{
-		SDL_Quit();
-		return 2;
-	}
-
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-	SDL_Texture* texture = SDL_CreateTexture(
-		renderer, 
-		SDL_PIXELFORMAT_RGBA8888,
-		SDL_TEXTUREACCESS_STATIC,
-		SCREEN_WIDTH,
-		SCREEN_HEIGHT
-		);
-
-	if (renderer == NULL)
-	{
-		cout << "Renderer not created." << endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 3;
-	}
-
-	if (texture == NULL)
-	{
-		cout << "Texture not created." << endl;
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		return 4;
-	}
-
-	Uint32* buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
-
-	// 0xFF = 255 in hexadecimal
-	memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
-
-	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_HEIGHT * sizeof(Uint32));
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
-
-	bool quit = false;
-	SDL_Event event;
-
-	while (!quit)
-	{
-		while (SDL_PollEvent(&event))
+		if (screen.processEvents() == false)
 		{
-			if (event.type == SDL_QUIT) {
-				quit = true;
-			}
+			break;
 		}
 	}
 	
-	delete[] buffer;
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyTexture(texture);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	screen.close();
 
 	return 0;
 }
---
+++ Particles/Screen.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include "SDL.h"
+
+namespace particle {
+
+class Screen
+{
+public:
+	const static int SCREEN_WIDTH = 800;
+	const static int SCREEN_HEIGHT = 600;
+
+private:
+	// By convention, when you have member variables, the prefix "m_" must be put before the variable.
+	// This will make clear they are instance variables, not local variables.
+	SDL_Window* m_window;
+	SDL_Renderer* m_renderer;
+	SDL_Texture* m_texture;
+	Uint32* m_buffer;
+
+public:
+	Screen();
+	bool init();
+	bool processEvents();
+	void close();
+};
+
+}
